08f5a000aef9880615115606a086a1fde8ed25fe
kwondh5217 <trewq231@naver.com>
2024-07-18 11:21:53 +0900

    feat: 시큐리티 설정 변경

--- src/main/java/com/e207/woojoobook/global/security/SecurityConfig.java
+++ src/main/java/com/e207/woojoobook/global/security/SecurityConfig.java
@@ -73,8 +73,7 @@ public class SecurityConfig {
 			.authorizeHttpRequests(request -> {
 				request.requestMatchers(AUTH_WHITELIST).permitAll()
 					.requestMatchers(HttpMethod.POST, "/users").permitAll()
-					.requestMatchers(HttpMethod.POST, "/users/emails").permitAll()
-					.requestMatchers(HttpMethod.PUT, "/users/emails").permitAll()
+					.requestMatchers("/users/emails/**").permitAll()
 					.anyRequest().authenticated();
 			})
 			.sessionManagement(sessionManagement ->
