b064152e86956411e46b2547a6759e135a4aa163
kwondh5217 <trewq231@naver.com>
2024-07-19 12:19:45 +0900

    feat: 회원 도서 리포지토리

---
+++ src/main/java/com/e207/woojoobook/domain/book/UserbookRepository.java
@@ -0,0 +1,6 @@
+package com.e207.woojoobook.domain.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserbookRepository extends JpaRepository<Userbook, Long> {
+}
